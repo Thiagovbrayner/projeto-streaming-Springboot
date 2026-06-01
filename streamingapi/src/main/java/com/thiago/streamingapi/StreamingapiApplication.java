a8b0491fa6f9076f1e5a9a93e7ee199e3bf84f8a
Thiagovbrayner <tvvb@cesar.school>
2026-06-01 00:28:16 -0300

    Logout admin

--- streamingapi/src/main/java/com/thiago/streamingapi/StreamingapiApplication.java
+++ streamingapi/src/main/java/com/thiago/streamingapi/StreamingapiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StreamingapiApplication {
+public class  StreamingapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StreamingapiApplication.class, args);
